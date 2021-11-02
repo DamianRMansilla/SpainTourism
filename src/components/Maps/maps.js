cc4915a2c5c0c35511c4d17dbd3a5c17b93f3ceb
DamianRMansilla <damianrmansilla85@gmail.com>
2021-11-02 13:22:09 +0100

    small update

--- src/components/Maps/maps.js
+++
@@ -1,8 +0,0 @@
-import "./maps.css";
-
-//const url = "https://www.google.com/maps/embed/v1/place?key=AIzaSyDRUSKddYh6lo9dY1A8SzsvgcfjCrLIGpY&q=Real+Casa+de+Correos,Madrid+Spain"
-
-
-export default function MapApi() {
-
-  }
